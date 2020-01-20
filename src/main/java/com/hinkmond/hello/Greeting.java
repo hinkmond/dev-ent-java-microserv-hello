5937383794b567bb23525447830508a67af5fc83
Hinkmond Wong <hinkmond@gmail.com>
2020-01-20 22:38:02 +0000

    Get rid of pom.xml (which is for a Spring Boot App) and add getHelloWorld() method.

--- src/main/java/com/hinkmond/hello/Greeting.java
+++ src/main/java/com/hinkmond/hello/Greeting.java
@@ -17,4 +17,14 @@ public class Greeting {
     public String getContent() {
         return content;
     }
+
+    public String getHelloWorld() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        Greeting greeting = new Greeting(0, "Greeting-Main");
+        System.out.println(greeting.getHelloWorld());
+    }
 }
+
